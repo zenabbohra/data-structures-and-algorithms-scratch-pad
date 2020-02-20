cb99753b869ce981a3e0b6c4f1b172a75c22a00e
Zenab <zenab.bohra19@gmail.com>
2020-02-20 11:58:43 +1100

    Adds Pseudocode to selection sort and expalins time complexity for best and worst case

--- src/big-o/sorting-algorithms/selection-sort.js
+++ src/big-o/sorting-algorithms/selection-sort.js
@@ -1,34 +1,36 @@
-/** STEPS to write code for selection sort
+/** Pseudocode for Selection sort
  * - Run a loop with variable i starting from the beginning of the array and going till end
- * - Run a loop inside of first loop with variable k going from i till end of the array
- * - Inside the nested loop check if element at k < at k+1, declare min variable
- * and store element at k as min until you find an element smaller than min, then
- * update min
- * - outside inner loop and inside outer loop, swap the element at i
- * with wherever the min element is
+ * - Declare min element index as i
+  * - Run a loop inside of first loop with variable k going from i + 1 till end of the array
+    * - Check if element at k < at minElemIndex, if yes, then update minElemIndx to k
+  * - outside inner loop and inside outer loop, swap the element at i with element at minElemIndx
  */
 
 const selectionSort = (arr) => {
-  for (let i = 0; i < arr.length - 1; i++) {
-    let minIndex = i;
-    for (let k = i; k < arr.length - 1; k++) {
-      if (arr[k + 1] < arr[minIndex]) {
-        minIndex = k+1;
+  for (let i = 0; i < arr.length; i++) {
+    let minElementIndex = i;
+    for (let k = i + 1; k < arr.length; k++) {
+      if (arr[k] < arr[minElementIndex]) {
+        minElementIndex = k;
       }
+      console.log(arr, k, i,);
     }
-    if (minIndex !== i) {
-      let temp = arr[minIndex];
-      arr[minIndex] = arr[i];
-      arr[i] = temp;
-    } else break;
+    if (minElementIndex !== i) {
+      // Swap in ES6+
+      [arr[i], arr[minElementIndex]] = [arr[minElementIndex], arr[i]];
+    }
   }
   return arr;
 };
 
-let arr = [0, 1, 3, 4, -8, 5, 5, 7];
+let arr = [0, 1, 3, 4, -8, 6, 5, 7];
+
 console.log(selectionSort(arr));
 
 //Time complexity of selection sort is O(n2)
 // Selection is not much better than bubble sort in terms of time complexity
-// but its better in terms of space complexity that you don't have to swap again and again
-// swap happens on;y once at the end of the loop (if required)
+// but its better in terms of space complexity as you don't have to swap again and again
+// swap happens only once at the end of the loop (if required)
+// But the difference between the two sorts occur when we have an almost or fully sorted array
+// then bubble sort runs at O(n) while selection is still O(n2) as it still checks in each iteration
+// if there is any minimum element or not

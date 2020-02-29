a897d6c5b48b3530fa83cede9196fdae69fcf187
Zenab <zenab.bohra19@gmail.com>
2020-03-01 08:56:27 +1100

    Adds a recursive function to calculate power of a number

---
+++ src/recursion/power.js
@@ -0,0 +1,17 @@
+/**
+ * @function power - takes a base number and an exponent number and returns the
+ * power of the base to the exponent
+ * @params {number} base
+ * @params {number} exponent
+ * @returns {number} result
+ */
+//PseudoCode
+  //
+  const power = (base, exponent) => {
+    if (exponent === 0) return 1;
+    return base * power(base, exponent -1);
+
+  };
+let base = 2;
+let exponent = 5;
+console.log(power(base, exponent));
